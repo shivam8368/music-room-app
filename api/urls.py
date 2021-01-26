b2679681830a40f86500031d7bf8bbbb9821be51
Shivam Kushwaha <shivamk8368@gmail.com>
2021-01-26 18:21:11 -0500

    my first commit

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from .views import JoinRoom, RoomView, CreateRoomView, GetRoom , UserInRoom
+
+
+urlpatterns = [
+    path('room/', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view()),
+    path('get-room', GetRoom.as_view()),
+    path('join-room', JoinRoom.as_view()),
+    path('user-in-room', UserInRoom.as_view()),
+]
